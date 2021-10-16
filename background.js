bce9a5b873f677a2c657f972a9a48db0a51a619a
Amardeep Ganguly <you@example.com>
2021-10-17 03:29:25 +0530

    added chrome notifications

--- background.js
+++ background.js
@@ -11,7 +11,16 @@ timer: time+1,
 chrome.action.setBadgeText({
     text:`${time+1}`
 })
+if(time%10==0)
+{console.log('Yeah!!')
+     this.registration.showNotification("StopWatch Extension",{
+body:"10 seconds has  passed!!!",
+icon:'icons/clock.png'
+     })
+}
 })
+//show the notification in every 10 seconds;
+
 })
 //update the time variable //
 //chrome.storage.sync.set()
